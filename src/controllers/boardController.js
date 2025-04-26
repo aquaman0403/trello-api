a7b86a452f673d62ab758e2e2cd22e8cae576ae1
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-04-26 12:04:10 +0700

    error handling middleware

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -7,14 +7,9 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body', req.body)
 
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
